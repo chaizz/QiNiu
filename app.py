34e85c432bbb10c70f7dfca9ab9512b161c4a134
chaizz <18037731392@163.com>
2021-05-11 19:48:18 +0800

    修改上传图片返回已有连接

--- app.py
+++ app.py
@@ -2,10 +2,9 @@
 import uuid
 import imghdr
 from flask import Flask
-from flask import render_template
-from flask import jsonify
+from flask import jsonify, make_response, render_template
 from flask import request
-from qiniu import Auth, put_data, etag, put_file
+from qiniu import Auth, put_data
 
 app = Flask(__name__)
 
@@ -17,34 +16,34 @@ def upload_file_qiniu(data):
     q = Auth(access_key, secret_key)
     bucket_name = 'cz-tuchuang'
     suffix_name = imghdr.what(None, data)
-    bad_pic_name = uuid.uuid5(uuid.NAMESPACE_DNS, "chaizz")
+    bad_pic_name = uuid.uuid1()
     pic_name = ''.join(str(bad_pic_name).split('-')) + '.' + suffix_name
-    print("pic_name", pic_name)
     token = q.upload_token(bucket_name, pic_name, 3600)
     ret1, ret2 = put_data(token, pic_name, data=data)
-    print('ret1:', ret1)
-    print('ret2:', ret2)
     # 判断是否上传成功
     if ret2.status_code != 200:
         raise Exception('文件上传失败')
-
     return ret1.get('key')
 
 
 @app.route('/', methods=['post', 'GET'])
 def upload_carimg():
     if request.method == 'GET':
-        return render_template('up_load_pic.html')
+        return jsonify(code=404)
     if request.method == 'POST':
         file_storage = request.files.get('file')
         data = file_storage.read()
         try:
-            pic_name = upload_file_qiniu(data)
-            External_link = "https://tc.chaizz.com/" + pic_name
-            return jsonify(ImgUrl=External_link)
+            file_name = upload_file_qiniu(data)
+            External_link = "https://tc.chaizz.com/" + file_name
+            res = make_response(jsonify(ImgUrl=External_link))
+            res.headers['Access-Control-Allow-Origin'] = '*'
+            res.headers['Access-Control-Allow-Method'] = '*'
+            res.headers['Access-Control-Allow-Headers'] = '*'
+            return res
         except Exception as e:
             return jsonify(errmsg='上传失败')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
